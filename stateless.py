4da864a16d797f1c76d2701b3aba2bbbbcfcca31
Daniel Adeyemo <adeyemodaniel10@gmail.com>
2018-07-17 22:49:38 +0100

    Rename file

---
+++ stateless.py
@@ -0,0 +1,6 @@
+"""FP for president."""
+
+# Functional code avoids side effects
+# Functions do not rely on, or change, data outside their scope
+
+# imperative style that creates side effects freely
