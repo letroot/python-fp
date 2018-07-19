605da6efe6430483660e91dd87bcf1e18a58476b
Daniel Adeyemo <adeyemodaniel10@gmail.com>
2018-07-19 17:19:33 +0100

    Add mutation of lists example

--- stateless.py
+++ stateless.py
@@ -5,15 +5,16 @@
 
 # imperative style that creates side effects freely
 text = "Twinkle twinkle"
+ls = [1, 2, 3, 5, 8, 13]
 
 def annotate():
     """Not a functional function"""
     global text
     text += " little star"
+    # append to list
 
 annotate()
 print(text)
 
 # Rewrite functionally
-# def f_annotate():
-#     pass
+# def annotate_fp():
